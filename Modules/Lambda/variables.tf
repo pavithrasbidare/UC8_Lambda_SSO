9acebf6d4dd07cc09afe7cb5df080c580ca25fdc
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-05 13:15:52 +0530

    Update variables.tf

--- Modules/Lambda/variables.tf
+++ Modules/Lambda/variables.tf
@@ -9,3 +9,13 @@ variable "image_name" {
   description = "The name of the Docker image for the Lambda function."
   type        = string
 }
+
+variable "user_pool_id" {
+  description = "The ID of the Cognito User Pool"
+  type        = string
+}
+
+variable "user_pool_client_id" {
+  description = "The ID of the Cognito User Pool Client"
+  type        = string
+}
