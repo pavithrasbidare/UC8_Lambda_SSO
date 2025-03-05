cc59773782648cce8d183bccbd856ce50a281318
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-05 13:16:55 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -11,6 +11,6 @@ module "lambda_api" {
 
   lambda_role_arn     = module.iam_role.lambda_role_arn
   image_name          = var.image_name
-  #user_pool_id        = module.cognito.user_pool_id
-  #user_pool_client_id = module.cognito.user_pool_client_id
+  user_pool_id        = module.cognito.user_pool_id
+  user_pool_client_id = module.cognito.user_pool_client_id
 }
