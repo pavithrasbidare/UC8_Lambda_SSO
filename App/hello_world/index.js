08a3f18ec9771a64e7caf7e752811a514fa74ad3
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-06 16:30:28 +0530

    Update index.js

--- App/hello_world/index.js
+++ App/hello_world/index.js
@@ -1,9 +1,9 @@
 exports.handler = async (event) => {
     // Cognito configuration for Lambda
     const cognitoConfig = {
-        userPoolId: 'us-east-1_Tnb6JZNY0',  // Cognito User Pool ID
-        userPoolClientId: '658bo70ggtnoa15puq3ek7j4o5',  // Cognito Client ID
-        region: 'us-east-1'  // Cognito region
+        userPoolId: 'us-west-2_OFnLIPph7',  // Cognito User Pool ID
+        userPoolClientId: '7hskt3uag35dd4o9fbbd26ljsb',  // Cognito Client ID
+        region: 'us-west-2'  // Cognito region
     };
  
     console.log("Cognito configuration:", cognitoConfig);
